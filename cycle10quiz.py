8b1d5c956fed0c4b7fbf740c2767e53f5a6cee63
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2022-03-11 13:03:28 -0500

    cycle10quiz

--- cycle10quiz.py
+++ cycle10quiz.py
@@ -1,42 +1,75 @@
 # author: LM (AMDG) 2/2/22
 # naming functio with one parameter
+from queue import Empty
+
+
 def average(lst):
-    #using enumerate to iterate through list of prices
+    # counter
+    total = 0
+    # using enumerate to iterate through list of prices
     for index, value in enumerate(lst):
-        value = value + lst[index]
-        #deletes the firstobject in list
-        del lst[index]
-
-        print(value)
+        total = total + value
+        new_average = total / 7
+    return new_average
 
 
-print(average([30, 40, 25, 55, 60, 80, 65]))
+print(average([30, 40, 55, 25, 60, 80, 65]))
 
+
+# function f0r reducing prices
 def reduced_Prices(lst):
-    empty= []
-    #using enumerate to go through list of prices
+    # where the reduced values will go in
+    empty = []
+    # using enumerate to go through list of prices
     for index, value in enumerate(lst):
-        #subtracting 5 from original value
+        # subtracting 5 from original value
         new_value = value - 5
-        #appending value to empty list
+        # appending value to empty list
         empty.append(new_value)
-        print(empty)
-    
-reduced_Prices([30, 40, 25, 55, 60, 80, 65])
-
-def sales(lst):
-    #using enumerate to iterate through sales list
-    for value in (lst):
-        for index, innervalue in value:
-            print(index, innervalue)
-    
+    return empty
 
 
+print(reduced_Prices([30, 40, 25, 55, 60, 80, 65]))
 
-def morethan40(prices):
-    #using enumerate to iterate through list
+
+# sales function
+def sales(prices, sales):
+    # counter
+    total = 0
+    # using enumerate to iterate through prices list, then multiplying the values by index 0 of sales list, then deleting it
+    for index, value, in enumerate(prices):
+        total = total + (value * sales[0])
+        del sales[0]
+    return total
+
+
+print(sales([30, 40, 25, 55, 60, 80, 65], [1, 3, 2, 5, 2, 1, 2]))
+
+
+# looks for values greater than 40and rediuces them by ten
+def morethan(prices):
+    # the new values will be stored here
+    newlst = []
+    # using enumerate to iterate through list
     for index, value in enumerate(prices):
-        #using condtional to sub=tract 10 from any value greater than 40
+        # using condtional to sub=tract 10 from any value greater than 40
         if value > 40:
             value = value - 10
-            print(value)
+            # value gets put into new list
+            newlst.append(value)
+    return newlst
+
+
+print(morethan([30, 40, 25, 55, 60, 80, 65]))
+
+
+def naming_pricing(prices, items):
+    # since the items list is 2 lists in one we have to use two 'for loops'
+    for group in items:
+        for index, value in enumerate(group):
+            print('{0} ${1}'.format(value, prices[0]))
+            # deletes price number at index 0 so it can continuously loop
+            del prices[0]
+
+
+naming_pricing([30, 40, 25, 55, 60, 80, 65],[["tee-shirt", 'long-sleeve', 'tanktop'], ['quarter-zip', 'pullover', 'full-zip', 'half-zip']])
